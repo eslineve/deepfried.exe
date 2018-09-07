cf4af5a5c36494b42b1895fb290af6c972434a58
eslineve <esli.neve@gmail.com>
2018-09-07 14:36:07 +0200

    added some of them deep fried noises

--- main.py
+++ main.py
@@ -6,61 +6,69 @@ from tkinter import ttk
 from PIL import ImageTk, Image
 
 import numpy as np
+import cv2
 
 def noise(image):
-      row,col,ch= image.shape
-      mean = 0
-      var = 0.1
+      imagecv = cv2.cvtColor(cv2.imread("Fry.jpg"), cv2.COLOR_BGR2RGB)
+
+      row,col,ch = imagecv.shape
+      mean = 1
+      var = 1000
       sigma = var**0.5
       gauss = np.random.normal(mean,sigma,(row,col,ch))
       gauss = gauss.reshape(row,col,ch)
-      noisy = image + gauss
-      return noisy
+      noisy = imagecv + gauss
+      print(type(noisy))
+      #im = Image.fromarray(noisy)
+      imgtk = ImageTk.PhotoImage(image = Image.fromarray(noisy.astype('uint8')))
+      return imgtk
 
 class Window(Frame):
-	def __init__(self, master=None):
-		Frame.__init__(self, master)
-		self.master = master
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
 
-		self.init_window()
+        self.init_window()
 
-	def init_window(self):
-		self.master.title("Fryer")
+    def init_window(self):
+        self.master.title("Fryer")
 
-		self.pack(fill=BOTH, expand=1)
-		FryButton  = Button(self, text="Fry one step", command=self.FryStep, width=10, height=1)
-		QuitButton = Button(self, text="Quit", command=self.Quit, width=10, height=1)
+        self.pack(fill=BOTH, expand=1)
+        FryButton  = Button(self, text="Fry one step", command=self.frystep, width=10, height=1)
+        QuitButton = Button(self, text="Quit", command=self.quit, width=10, height=1)
 
-		self.progress = ttk.Progressbar(self, orient="horizontal",
+        self.progress = ttk.Progressbar(self, orient="horizontal",
                                         length=310, mode="determinate")
 
-		image = ImageTk.PhotoImage(Image.open("Fry.jpg"))
-		label = Label(image=image, width=530, height=530)
-		label.image = image # keep a reference!
-		label.place(x=10, y=10)
+        self.image = ImageTk.PhotoImage(Image.open("Fry.jpg"))
+        self.label = Label(image=self.image, width=530, height=530)
+        self.label.image = self.image # keep a reference!
+        self.label.place(x=10, y=10)
 
-		FryButton.place(x=340, y=560)
-		QuitButton.place(x=440, y=560)
-		self.progress.place(x=10, y=562)
+        FryButton.place(x=340, y=560)
+        QuitButton.place(x=440, y=560)
+        self.progress.place(x=10, y=562)
 
-		self.progress["value"] = 0
-		self.progress["maximum"] = 100
+        self.progress["value"] = 0
+        self.progress["maximum"] = 100
 
-	def FryStep(self):
-		print("Fry that picture")
+    def frystep(self):
+        print("Fry that picture")
+        new_image = noise(self.image)
+        self.label.configure(image=new_image)
+        self.label.image = new_image
+        self.progress["value"] += 5
 
-		self.progress["value"] += 5
-
-	def Quit(self):
-		exit()
+    def quit(self):
+        exit()
 
 
 if __name__ == '__main__':
-	print("Hello World my old friend")
+    print("Hello World my old friend")
 
-	root = Tk()
-	root.resizable(width=False, height=False)
-	root.geometry("550x600")
-	app = Window(root)
+    root = Tk()
+    root.resizable(width=False, height=False)
+    root.geometry("550x600")
+    app = Window(root)
 
-	root.mainloop()
+    root.mainloop()
